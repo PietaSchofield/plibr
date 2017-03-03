1c1511ace03f5e52aaeed21d5acc05526ee923ab
Pieta Schofield <pieta.schofield@me.com>
2017-03-03 10:18:42 +0000

    periodic

--- R/hmAnalysis.R
+++ R/hmAnalysis.R
@@ -5,12 +5,12 @@ hmAnalysis <- function(exprMat,rowV=c("correlation","mcquitty"),colV=c("canberra
                        ncuts=6,filename=NA){
    ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
                   main=paste0("PGS vs Untreated in LPS and Untreated CTR Sbmples \n",
-                       length(allSig)," significant DE genes "),filename=".tmp.pdf")
+                       length(rownames(exprMat))," significant DE genes "),filename=".tmp.pdf")
    ct <- as.data.frame(dendextend::cutree(as.dendrogram(ah$Rowv),ncuts))
    colnames(ct) <- "Group"
    ct$Group <- as.factor(ct$Group)
    ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
                   annRow=ct, main=paste0("PGS vs Untreated in LPS and Untreated CTR Samples \n",
-                        length(allSig)," significant DE genes "),filename=filename)
+                        length(rownames(exprMat))," significant DE genes "),filename=filename)
    return(ct)
 }
