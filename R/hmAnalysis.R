7e7eb3dddd3439c7c21f17e9b799f5875f719f3d
Pieta Schofield <pieta.schofield@me.com>
2017-03-03 09:15:11 +0000

    periodic

---
+++ R/hmAnalysis.R
@@ -0,0 +1,16 @@
+#' use heatmap and tree cutting
+#'
+#' @export
+hmAnalysis <- function(exprMat,rowV=c("correlation","mcquitty"),colV=c("canberra","average"),
+                       ncuts=6,filename=NA){
+   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
+                  main=paste0("PGS vs Untreated in LPS and Untreated CTR Sbmples \n",
+                       length(allSig)," significant DE genes "),filename=".tmp.pdf")
+   ct <- as.data.frame(dendextend::cutree(as.dendrogram(ah$Rowv),ncuts))
+   colnames(ct) <- "Group"
+   ct$Group <- as.factor(ct$Group)
+   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
+                  annRow=ct, main=paste0("PGS vs Untreated in LPS and Untreated CTR Samples \n",
+                        length(allSig)," significant DE genes "),filename=filename)
+   return(ct)
+}
