58dfaef310f9eba8ba4644e388209a2a1e5ef6cb
Pieta Schofield <pieta.schofield@me.com>
2017-03-03 11:49:25 +0000

    periodic

--- R/hmAnalysis.R
+++ R/hmAnalysis.R
@@ -2,15 +2,13 @@
 #'
 #' @export
 hmAnalysis <- function(exprMat,rowV=c("correlation","mcquitty"),colV=c("canberra","average"),
-                       ncuts=6,filename=NA){
-   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
-                  main=paste0("PGS vs Untreated in LPS and Untreated CTR Sbmples \n",
-                       length(rownames(exprMat))," significant DE genes "),filename=".tmp.pdf")
+                       ncuts=6,filename=NA,mainTitle="Heatmap"){
+   mainTitle <- paste0(mainTitle,"\n",length(rownames(exprMat))," significant DE genes")
+   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV, main=mainTitle, filename=".tmp.pdf")
    ct <- as.data.frame(dendextend::cutree(as.dendrogram(ah$Rowv),ncuts))
    colnames(ct) <- "Group"
    ct$Group <- as.factor(ct$Group)
-   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV,
-                  annRow=ct, main=paste0("PGS vs Untreated in LPS and Untreated CTR Samples \n",
-                        length(rownames(exprMat))," significant DE genes "),filename=filename)
+   ah <- NMF::aheatmap(exprMat, scale="row",Rowv=rowV, Colv=colV, annRow=ct, main=mainTitle,
+                       filename=filename)
    return(ct)
 }
