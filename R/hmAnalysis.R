1d30ed7952e60685cb44ae60d69b4bb3f5e4c350
Pieta Schofield <pieta.schofield@me.com>
2017-04-21 17:49:50 +0100

    periodic

--- R/hmAnalysis.R
+++ R/hmAnalysis.R
@@ -1,10 +1,10 @@
 #' use heatmap and tree cutting
 #'
 #' @export
-hmAnalysis <- function(exprMat,rowV=NA,colV=NA,
+hmAnalysis <- function(exprMat,rowV=NULL,colV=NULL,height=NA,width=NA,ch=NA,cw=NA,leg=T,al=T,
                        ncuts=6,filename=NA,mainTitle="Heatmap",cr=1.5,cc=0.5){
    mainTitle <- paste0(mainTitle,"\n",length(rownames(exprMat))," significant DE genes")
-   if(!is.na(rowV)){
+   if(is.dendrogram(rowV)){
      ct <- as.data.frame(dendextend::cutree(rowV,ncuts))
      colnames(ct) <- "Group"
      ct$Group <- as.factor(ct$Group)
@@ -13,10 +13,13 @@ hmAnalysis <- function(exprMat,rowV=NA,colV=NA,
      ctr <- ctr[,-1]
      anR <- ct$Group
      names(anR) <- rownames(anR)
-     ah <- NMF::aheatmap(exprMat[,-c(1:3)], scale="row",Rowv=rowV, Colv=colV, annRow=ct, 
-                       main=mainTitle, filename=filename,cexRow=cr,cexCol=cc,treeheight=30)
+     ah <- NMF::aheatmap(exprMat[,-c(1:3)], scale="row",Rowv=rowV, Colv=colV, annRow=ct,
+                         annLegend=al,
+                         height=height,width=width,cellwidth=cw,cellheight=ch,legend=leg, 
+                         main=mainTitle, filename=filename,cexRow=cr,cexCol=cc,treeheight=30)
    }else{
-     ah <- NMF::aheatmap(exprMat[,-c(1:3)], scale="row",Rowv=rowV, Colv=colV, 
+     ah <- NMF::aheatmap(exprMat[,-c(1:3)], scale="row",Rowv=rowV, Colv=colV,legend=leg,
+                         height=height,width=width,cellwidth=cw,cellheight=ch, 
                          main=mainTitle, filename=filename,cexRow=cr,cexCol=cc,treeheight=30)
    }
    return(ctr)
