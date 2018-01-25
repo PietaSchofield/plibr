5978e5fb4a97f3a3cd74670f5da790ac7b0607fb
Pieta Schofield <pieta.schofield@me.com>
2018-01-25 13:43:02 +0000

    Automated Thu Jan 25 13:43:02 2018

--- R/multiplot.R
+++ R/multiplot.R
@@ -13,7 +13,7 @@
 #'     3 will go all the way across the bottom.
 #'
 #' @export
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+multiplot <- function(..., plotlist=NULL, file, cols=1, lout=NULL) {
   require(grid)
 
   # Make a list from the ... arguments and plotlist
@@ -22,26 +22,25 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   numPlots = length(plots)
 
   # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
+  if (is.null(lout)) {
     # Make the panel
     # ncol: Number of columns of plots
     # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+    lout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
                     ncol = cols, nrow = ceiling(numPlots/cols))
   }
 
  if (numPlots==1) {
     print(plots[[1]])
-
-  } else {
+ } else {
     # Set up the page
     grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    pushViewport(viewport(layout = grid.layout(nrow(lout), ncol(lout))))
 
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      matchidx <- as.data.frame(which(lout == i, arr.ind = TRUE))
 
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
