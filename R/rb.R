b12b807d724bd3b4a2320b04c27d1df365dde979
Pieta Schofield <pieta.schofield@me.com>
2018-02-28 19:01:36 +0000

    Automated Wed Feb 28 19:01:36 2018

--- R/rb.R
+++ R/rb.R
@@ -5,9 +5,16 @@
 #' @param rstudio
 #'
 #' @export
-rb <- function(pkgName="plib_ps", baseDir=file.path(Sys.getenv("HOME"),"Projects")){
+rb <- function(projName="plib_ps", pkgName="package", 
+               baseDir=file.path(Sys.getenv("HOME"),"Projects")){
   curd <- getwd()
-  setwd(file.path(baseDir,pkgName))
+  if(projName!="plib_ps"){
+    wd <- file.path(baseDir,projName,pkgName)
+  }else{
+    wd <- file.path(baseDir,projName)
+  }
+  print(wd)
+  setwd(wd)
   devtools::document()
   devtools::load_all()
   devtools::install(reload=T)
