28969024285e656cd270f76ed580d1175a2a6708
Pieta Schofield <pieta.schofield@me.com>
2018-06-27 12:13:11 +0100

    Fix to commit new files as well as modified filesWed Jun 27 12:13:11 2018

--- R/autoCommit.R
+++ R/autoCommit.R
@@ -8,7 +8,8 @@
 #' @param subdir names of subdirectories that include code
 #'
 #' @export
-autoCommit <- function(projDir="~/Projects",pat="*",subdir="^(Code|R|package)$"){
+autoCommit <- function(projDir="~/Projects",pat="*",subdir="^(Code|R|package)$",
+                       commitMessage="Automated "){
   dirs <- list.files(projDir,pattern=pat, include.dirs=T, no..=T, full=T)
   retValue <- writeLines(unlist(lapply(dirs,function(d){
     pushRes <- "No Changes"
@@ -22,21 +23,30 @@ autoCommit <- function(projDir="~/Projects",pat="*",subdir="^(Code|R|package)$")
       }else{
         repDir <- cdir
       }
-      system(paste0("cd ",repDir,"; git fetch origin"),intern=F)
       repo <- git2r::repository(repDir)
+      #
+      # Kludge to get round lake of protocol for fetch
+      #
+      system(paste0("cd ",repDir,"; git fetch origin"),intern=F)
       stat <- git2r::status(repo)
-      if(length(stat$unstaged)>0){
+      if(length(stat$unstaged)>0 | length(stat$untracked)>0 ){
         git2r::add(repo=repo,file.path("*.*"))
       }
       stat <- git2r::status(repo)
       if(length(stat$staged)>0){
-        git2r::commit(repo=repo,message=paste0("Automated ",date()))
+        git2r::commit(repo=repo,message=paste0(commitMessage," ",date()))
+        #
+        # Kludge to get round lake of protocol for push
+        #
         if(system(paste0("cd ",repDir,"; git push"),intern=F)==0){
           pushRes <- "Pushed"
         }else{
           pushRes <- "Push Error"
         }
       }
+      #
+      # Kludge to get round lake of protocol for pull
+      #
       system(paste0("cd ",repDir,"; git pull"),intern=T)
       paste0(pushRes," ",repDir)
     })
