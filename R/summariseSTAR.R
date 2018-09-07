177c3ab567e331ebf37fb5991ad19298b5aa199b
Pieta Schofield <pieta.schofield@me.com>
2018-09-07 18:07:27 +0100

    Automated  Fri Sep  7 18:07:27 2018

--- R/summariseSTAR.R
+++ R/summariseSTAR.R
@@ -21,5 +21,16 @@ summariseSTAR <- function(projName,fileList,localDir=T){
   names(ret) <- gsub("Log[.]final[.]out$","",basename(fileList))
   ret <- plyr::ldply(ret)
   rownames(ret) <- make.names(ret[,1],unique=T)
-  ret[,-1]
+  ret <- ret[,-1]
+  colnames(ret) <- c("Start_Job","Start_Mapping","Finish_Job","Mapping_Speed_MRPH",
+                     "Input_Reads","Ave_Read_Length",
+                     "UNIQUE","No_Unique_Map","Perc_Unique_Map","Ave_Map_Length",
+                     "Splice_Total","Splice_Annotated","Splice_GT_AG","Splice_GC_AG",
+                     "Splice_AT_AC","Splice_Non-canonical",
+                     "Mismatch_rate_Perc","Deletion_rate","Ave_Del_Length","Insert_rate",
+                     "Ave_Ins_Length",
+                     "MULTIMAP","Multimap_read","Perc_Multimap","Overmap_reads","Perc_Overmap",
+                     "UNMAPPED","Unmap_mismatches","Unmap_length","Unmap_other",
+                     "CHIMERIC","Chimeric_reads","Perc_Chimeric")
+  ret
 }
