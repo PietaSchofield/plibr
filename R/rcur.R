7a25904b609b8b4b05b53463c46beb24e10f8e6d
Pieta Schofield <pieta.schofield@me.com>
2019-04-24 13:55:22 +0100

    Automated  Wed Apr 24 13:55:22 2019

--- R/rcur.R
+++ R/rcur.R
@@ -13,7 +13,8 @@
 #' @export
 rc <- function(fileName=.curFile,projName=.projName,codeDir="GitLab",gitRepo="liverpool",
                sysRoot=Sys.getenv("HOME"), topDir="public_html", setGH=F, toPDF=F,toDOCX=F,
-               toHTML=T,upload=T,outRoot=".tmp"){
+               toHTML=T,upload=T,outRoot=".tmp",outDocxPath=NULL,userid="pietas",
+               secret=Sys.getenv("MY_SECRET")){
   if(tolower(Sys.info()["sysname"])=="windows"){
     sysRoot="M:"
   }
@@ -36,6 +37,10 @@ rc <- function(fileName=.curFile,projName=.projName,codeDir="GitLab",gitRepo="li
   if(toDOCX){
     docxFile <- rmarkdown::render(input=infile,output_dir=outpath,
                                output_format="bookdown::word_document2")
+    if(!is.null(outDocxPath)){
+      RCurl::ftpUpload(docxFile,paste0("ftp://",userid,":",secret,"@pcftp.liv.ac.uk:21",
+                                       file.path(outDocxPath,basename(docxFile))))
+    }
   }
   if(toHTML){
     htmlFile <- rmarkdown::render(input=infile,output_dir=outpath,
