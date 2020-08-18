ea9663c7bc45a2a2008c587aa42ebb0df26a4939
Pieta Schofield <pieta.schofield@me.com>
2020-08-18 10:09:30 +0100

    Automated  Tue Aug 18 10:09:30 2020

--- R/opendb.R
+++ R/opendb.R
@@ -5,11 +5,15 @@ openDB <- function(db,credir=file.path(Sys.getenv("HOME"),".config"),
                    credfile=file.path(credir,"psqlcred.yml")){
   pg <- config::get(file=credfile)
   cd <- pg[[db]]
-  RPostgres::dbConnect(RPostgres::Postgres(),
+  tryCatch({
+    RPostgres::dbConnect(RPostgres::Postgres(),
     dbname=cd$database,
     host=cd$server,
     port=cd$port,
     user=cd$uid,
     password=cd$pwd
- )
+  )},
+  error=function(e){
+    return(F)
+  })
 }
