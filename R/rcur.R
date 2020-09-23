a0a3d40bca8a3e5a467c87893e9852da2ba8ca95
Pieta Schofield <pieta.schofield@me.com>
2020-09-23 09:35:33 +0100

    Automated  Wed Sep 23 09:35:33 2020

--- R/rcur.R
+++ R/rcur.R
@@ -3,6 +3,7 @@
 #' renders the named document and puts the output html file in the correct location in my filespace
 #' This currently assumes that I have mounted my troodon home directory to the local host via sshfs
 #' which is available from OSXFUSE. Default paths are configured for my code locations.
+#' 
 #'
 #' Been making changes to this to use bookdown document styles
 #'
@@ -11,86 +12,61 @@
 #' @param codeDir code tree path
 #' @param sysRoot home directory
 #' @param userid User id 
-#' @param topDir output home directory on god
 #' @param outRoot local temporary file creation location
 #'
 #' @export
 rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,
-               sysRoot=Sys.getenv("HOME"), userid=Sys.getenv("USER"), 
+               sysRoot=Sys.getenv("HOME"), user=Sys.getenv("USER"), 
                codeDir=file.path(sysRoot,"GitLab",gitRepo),hostname="dh174037.liv.ac.uk",
-               godRoot="public_html", outRoot=file.path(sysRoot,".tmp"),
-               livRoot=file.path("/","var","www","html"),
-               pphRoot=file.path("/","opt","shiny-server","samples","sample-apps"),
+               outRoot=file.path(sysRoot,".tmp"),
+               htmlRoot=file.path("/","var","www","html"),
+               shinyRoot=file.path("/","opt","shiny-server","samples","sample-apps"),
+               docRoot=file.path(sysRoot,"Projects",.projName),
                setHome=F, toPDF=F,toDOCX=F, toHTML=T,
-               godUP=F, livUP=T, pphUP=F,over=T,ext="Rmd",livLocal=T){
+               htmlUP=T, shinyUP=F,pdfUP=F,docUP=F,ext="Rmd"){
   if(!is.null(projName)){
     codePath <- file.path(codeDir,projName)
-    godPath <- file.path(godRoot,gitRepo,projName)
-    livPath <- file.path(livRoot,gitRepo,projName)
-    pphPath <- file.path(pphRoot,projName)
+    htmlPath <- file.path(htmlRoot,gitRepo,projName)
+    shinyPath <- file.path(shinyRoot,gitRepo,projName)
     outPath <- file.path(outRoot,projName)
   }else{
     codePath <- file.path(codeDir)
     outPath <- file.path(outRoot,gitRepo)
-    godPath <- file.path(godRoot,gitRepo)
-    pphPath <- file.path(pphRoot)
-    livPath <- file.path(livRoot,gitRepo)
+    shinyPath <- file.path(shinyRoot,gitRepo)
+    htmlPath <- file.path(htmlRoot,gitRepo)
   }
   if(setHome){
-    godPath <- godRoot 
-    livPath <- livRoot
-    pphPath <- pphRoot
-    godFile <- file.path(godPath,"index.html")
-    livFile <- file.path(livPath,"index.html")
-    pphFile <- file.path(pphPath,"index.html")
+    htmlFileName <- file.path(htmlPath,"index.html")
   }else{
-    godFile <- file.path(godPath,paste0(fileName,".html"))
-    livFile <- file.path(livPath,paste0(fileName,".html"))
-    if(toHTML){
-      pphFile <- file.path(pphPath,paste0(fileName,".html"))
-    }else{
-      pphFile <- file.path(pphPath,paste0(fileName,".",ext))
-    }
+    htmlFileName <- file.path(htmlPath,paste0(fileName,".html"))
+    shinyFileName <- file.path(shinyPath,paste0(fileName,".",ext))
+    docFileName <- file.path(docPath,fileName)
   }
   dir.create(outPath,showW=F,recur=T)
   infile <- file.path(codePath,paste0(fileName,".",ext))
   if(toDOCX){
     docxFile <- rmarkdown::render(input=infile,output_dir=outPath,
                                output_format="bookdown::word_document2")
+    if(docUP){
+       system(paste0("scp ",infile," ",paste0(user,"@",hostname,":",docFileName,".docx")))
+    }
   }
   if(toHTML){
     htmlFile <- rmarkdown::render(input=infile,output_dir=outPath,
                                output_format="bookdown::html_document2")
+    if(htmlUP){
+      system(paste0("scp ",htmlFile," ",paste0(user,"@",hostname,":",htmlFileName)))
+    }
   }
   if(toPDF){
     pdfFile <- rmarkdown::render(input=infile,output_dir=outPath,
                                  output_format="bookdown::pdf_document2")
-  }
-  if(godUP){
-    system(paste0("scp ",htmlFile," ",paste0("pieta@pieta.me:",godFile)))
-  }
-  if(livUP){
-    if(livLocal){
-      dir.create(livPath,showW=F,recur=T)
-      file.copy(htmlFile,livFile,overwrite=over)
-    }else{
-      system(paste0("scp ",htmlFile," ",paste0("pietas@",hostname,":",livFile)))
+    if(pdfUP){
+       system(paste0("scp ",infile," ",paste0(user,"@",hostname,":",docFileName,".pdf")))
     }
   }
-  if(pphUP){
-    if(file.exists(pphRoot)){
-      dir.create(pphPath,showW=F,recur=T)
-      if(toHTML){
-        file.copy(htmlFile,pphFile,overwrite=over)
-      }else{
-        file.copy(infile,pphFile,overwrite=over)
-      }
-    }else{
-      if(toHTML){
-        system(paste0("scp ",htmlFile," ",paste0("pietas",hostname,":",pphFile)))
-      }else{
-        system(paste0("scp ",infile," ",paste0("pietas",hostname,":",pphFile)))
-      }
-    }
+  if(shinyUP){
+    system(paste0("scp ",infile," ",paste0(user,"@",hostname,":",shinyFileName)))
   }
+
 }
