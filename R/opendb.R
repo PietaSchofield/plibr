ce17ede11d24a40916ea3a9ecf227d79371e8ddc
Pieta Schofield <pieta.schofield@me.com>
2022-05-31 10:18:18 +0100

    Automated  Tue May 31 10:18:18 2022

--- R/opendb.R
+++ R/opendb.R
@@ -5,19 +5,32 @@
 #' @param credfile the name of the credentials file if not standard
 #'
 #' @export
-openDB <- function(db,credir=file.path(Sys.getenv("HOME"),".config"),
-                   credfile=file.path(credir,"psqlcred.yml")){
-  pg <- config::get(file=credfile)
+openDB <- function(db,credir=NULL, credfile="psqlcred.yml"){
+  credFile <- file.path(credir,credfile)
+  pg <- config::get(file=credFile)
   cd <- pg[[db]]
-  tryCatch({
-    RPostgres::dbConnect(RPostgres::Postgres(),
-    dbname=cd$database,
-    host=cd$server,
-    port=cd$port,
-    user=cd$uid,
-    password=cd$pwd
-  )},
-  error=function(e){
-    return(F)
-  })
+  if(!is.null(cd$database)){
+    tryCatch({
+      RPostgres::dbConnect(RPostgres::Postgres(),
+        dbname=cd$database,
+        host=cd$server,
+        port=cd$port,
+        user=cd$uid,
+        password=cd$pwd
+    )},
+    error=function(e){
+      return(F)
+    })
+  }else{
+     tryCatch({
+      RPostgres::dbConnect(RPostgres::Postgres(),
+        host=cd$server,
+        port=cd$port,
+        user=cd$uid,
+        password=cd$pwd
+    )},
+    error=function(e){
+      return(F)
+    })
+  }
 }
