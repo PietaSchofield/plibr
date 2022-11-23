90d876f20c59843109aa0d96a446a0fcf3caf167
Pieta Schofield <pieta.schofield@me.com>
2022-11-23 15:18:33 +0000

    still problem with autoCommit

--- R/rcur.R
+++ R/rcur.R
@@ -4,26 +4,26 @@
 #' dundee version to a version that worked with GoDaddy and then worked at Manchester CRUK and then cope
 #' with the changes at CRUK and the whole reconfiguration due to the fire and then the  move to Liverpool
 #' and the introduction of the virtual machine at Liverpook running a shiny server and the final dropping
-#' of GoDaddy. 
+#' of GoDaddy.
 #'
 #' @param projDir project name
 #' @param fileName file name
 #' @param codeDir code tree path
 #' @param sysRoot home directory
-#' @param userid User id 
+#' @param userid User id
 #' @param outRoot local temporary file creation location
 #'
 #' @export
-rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,
-               sysRoot=Sys.getenv("HOME"), user=Sys.getenv("USER"), 
-               codeDir=file.path(sysRoot,"GitLab",gitRepo),hostname="dh174037.liv.ac.uk",
-               outRoot=file.path(sysRoot,".tmp"),
+rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,sysRoot=.sysRoot,
+               user=Sys.getenv("USER"),
+               hostname=.hostName,
                htmlRoot=file.path("/","var","www","html"),
                shinyRoot=file.path("/","u1","shiny-server","samples","apps"),
-               onedrive=file.path(sysRoot,"OneDrive","me","html"),
-               docRoot=file.path(sysRoot,"Projects",projName),
                setHome=F, toPDF=F,toDOCX=F, toHTML=T,setRepo=T,setProj=T,toShiny=F,
                htmlUP=T, rmdUP=F,pdfUP=F,docUP=F,ext="Rmd",dbg=F){
+  codeDir=file.path(sysRoot,"GitLab",gitRepo)
+  outRoot=file.path(sysRoot,".tmp")
+  docRoot=file.path(sysRoot,".tmp")
   codePath <- file.path(codeDir)
   outPath <- file.path(outRoot,gitRepo)
   docPath <- file.path(docRoot)
@@ -32,11 +32,9 @@ rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,
   }else{
     htmlPath <- file.path(htmlRoot)
   }
-  odPath <- file.path(onedrive)
   if(setRepo){
     htmlPath <- file.path(htmlPath,gitRepo)
     docPath <- file.path(docPath,gitRepo)
-    odPath <- file.path(odPath,gitRepo)
   }
   if(setProj){
     codePath <- file.path(codePath,projName)
@@ -47,14 +45,11 @@ rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,
       htmlPath <- file.path(htmlPath,projName)
     }
     outPath <- file.path(outPath,projName)
-    odPath <- file.path(odPath,projName)
   }
   if(setHome){
     htmlFileName <- file.path(htmlPath,"index.html")
-    odFileName <- file.path(odPath,"index.html")
   }else{
     htmlFileName <- file.path(htmlPath,paste0(fileName,".html"))
-    odFileName <- file.path(odPath,paste0(fileName,".html"))
     if(rmdUP){
       shinyFileName <- file.path(htmlPath,paste0(fileName,".Rmd"))
     }else{
@@ -79,9 +74,6 @@ rc <- function(fileName=.curFile,projName=.projName,gitRepo=.gitRepo,
                                output_format="bookdown::html_document2")
     if(htmlUP){
       system(paste0("scp ",htmlFile," ",paste0(user,"@",hostname,":",htmlFileName)))
-      if(file.exists(odPath)){
-        system(paste0("cp ",htmlFile," ",odFileName))
-      }
     }
   }
   if(toPDF){
