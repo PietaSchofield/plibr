edce018913cd094f8bca43947d29823d2d71bcf7
Pieta <pieta.schofield@me.com>
2024-09-19 17:38:03 +0100

    pp

--- R/setcurrent.R
+++ R/setcurrent.R
@@ -1,13 +1,13 @@
 #' set current month
 #' 
 #' @export
-setcurrent <- function(fileName,dir_path="/srv/http/uol/notes",linkName="current"){
+setcurrent <- function(fileName,in_path="/srv/http/uol/notes",out_path=in_path,linkName="current"){
   
   # Full path to the target file
-  target_path <- file.path(dir_path, paste0(fileName,".html"))
+  target_path <- file.path(in_path, paste0(fileName,".html"))
   
   # Path for the symbolic link
-  link_path <- file.path(dir_path, paste0(linkName,".html"))
+  link_path <- file.path(out_path, paste0(linkName,".html"))
   if(file.exists(link_path)) unlink(link_path)  
   # Create the symbolic link
   file.symlink(from = target_path, to = link_path)
