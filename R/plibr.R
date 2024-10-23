5f3b356d4fbfd5b2f4234deb76ec49510067271c
Pieta <pieta.schofield@me.com>
2024-10-23 12:25:40 +0100

    pp

---
+++ R/plibr.R
@@ -0,0 +1,17 @@
+#' plibr
+#'
+#' Latest iteration of my convenience function code for managing Rmarkdown wrangling for creating
+#' electronic notebooks
+#'
+#' A library of convenience functions to make life easier while produce workday notebooks to record what
+#' I have been doing in particular submitting and running processing jobs on the cluster. This library
+#' used to exist as two separate libraries, my original pietalib that I started at Dundee and a CRUKlib
+#' that I started at CRUK Manchester. However it makes more sense to combine these now.
+#'
+#' Most of this is redundant code now but I am loathed to lose it certainly all the batch-job submission
+#' management for bioinformatics jobs is not distant history
+#'
+#' @docType _PACKAGE
+#' @name plibr
+#'
+NULL
