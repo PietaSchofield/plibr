872ff67780aa0b9d1e2cc9119460148946d77de0
Pieta <pieta.schofield@me.com>
2025-01-23 16:12:21 +0000

    pp

--- R/displayURL.R
+++ R/displayURL.R
@@ -1,12 +1,10 @@
 #' display a web page nicely in chrome
 #'
 #' @export
-displayURL <- function(url) 
-{
-    chrome_path <- "/usr/bin/google-chrome-stable"
-    profile_path <- "/home/pietas/.config/google-chrome"
-    system(paste0("\"", chrome_path, "\" --user-data-dir=\"", 
-        profile_path, "\" --profile-directory=\"Default\" ", 
-        url))
+displayURL <- function(urlpath="http://localhost", browser_path="/usr/bin/brave") {
+  if(F){
+    urlpath="http://localhost"
+  }
+  system2(browser_path,arg=urlpath,wait=F,stdout=NULL,stderr=NULL)
 }
 
