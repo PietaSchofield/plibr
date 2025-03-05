eceb2e97c700a0b39b036f35efc7c7109097cf49
Pieta <pieta.schofield@me.com>
2025-03-05 13:18:22 +0000

    periodic

--- R/displayURL.R
+++ R/displayURL.R
@@ -1,9 +1,12 @@
 #' display a web page nicely in chrome
 #'
 #' @export
-displayURL <- function(urlpath="http://localhost", browser_path="/usr/bin/librewolf") {
-  if(F){
-    urlpath="http://localhost"
+displayURL <- function(urlpath=NULL, browser_path=NULL) {
+  if (is.null(urlpath)) {
+    urlpath <- "http://localhost/uol"
+  }
+  if (is.null(browser_path)) {
+    browser_path <- getOption("browser_path", "/usr/bin/librewolf") # Default if not set
   }
   system2(browser_path,arg=urlpath,wait=F,stdout=NULL,stderr=NULL)
 }
