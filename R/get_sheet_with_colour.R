4e1871d00feaccfa00f148351cf158d5afec2f46
Pieta <pieta.schofield@me.com>
2026-04-26 13:10:30 +0100

    2026-04-26 13:10 tweaking colour read of excel sheets

--- R/get_sheet_with_colour.R
+++ R/get_sheet_with_colour.R
@@ -5,11 +5,11 @@
 get_sheet_with_colour <- function(file, sheet) {
 
   # read the sheet data normally
-  dat <- read_excel(file, sheet = sheet)
+  dat <- readxl::read_excel(file, sheet = sheet, col_types="text")
 
   # read cell metadata
-  cells <- xlsx_cells(file, sheets = sheet)
-  formats <- xlsx_formats(file)
+  cells <- tidyxl::xlsx_cells(file, sheets = sheet)
+  formats <- tidyxl::xlsx_formats(file)
 
   # colour lookup table
   fills <- formats$local$fill$patternFill$fgColor$rgb
